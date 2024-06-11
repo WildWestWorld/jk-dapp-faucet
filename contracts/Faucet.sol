baf13d13e9fa085b7aa7c6670885673c0c4a9e4e
JOTARO <380686356@qq.com>
2024-06-11 13:09:19 +0800

    完成添加external

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -2,9 +2,19 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
+    address[] public funders;
 
-    receive() external payable{
-        
+    receive() external payable {}
+    function addFunds() external payable {
+        funders.push(msg.sender);
     }
 
+    function getAllFunders() public view returns (address[] memory) {
+        return funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns (address) {
+        address[] memory _funders = getAllFunders();
+        return _funders[index];
+    }
 }
