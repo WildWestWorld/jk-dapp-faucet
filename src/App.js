ff79adf9177fa03d3d9c941f6feb89072610e1ff
JOTARO <380686356@qq.com>
2024-06-21 13:26:20 +0800

    完成加载合约(重要)

--- src/App.js
+++ src/App.js
@@ -2,29 +2,32 @@ import { useEffect, useState } from "react";
 import "./App.css"
 import Web3 from "web3";
 import detectEthereumProvider from "@metamask/detect-provider";
-import Contract from '@truffle/contract';
+import { loadContract } from "./utils/load-contract";
 
 
 function App() {
 
   const [web3Api, setWeb3Api] = useState({
     provider: null,
-    web3: null
+    web3: null,
+    contract: null
   })
 
   const [account, setAccount] = useState(null)
 
   useEffect(() => {
-    console.log(Contract)
     const loadProvider = async () => {
 
 
       const provider = await detectEthereumProvider();
+      const contract = await loadContract("Faucet", provider)
+
       if (provider) {
 
         setWeb3Api({
           web3: new Web3(provider),
-          provider
+          provider,
+          // contract
         })
       } else {
         console.error('Please,install Metamask')
--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,8 @@
 
 module.exports = {
-
+  contracts_build_directory: "./public/contracts",
 
   networks: {
-
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 7545,            // Standard Ethereum port (default: none)
